1f7aef3b0b41fe22375193135c7515a13c256d52
Discosultan <j2v@neti.ee>
2015-01-28 18:17:27 +0200

    Automatic nuget publish script.

---
+++ Tools/PublishNuget.debug.ps1
@@ -0,0 +1,41 @@
+# Define variables.
+$consoleId = 'Paradox.Console'
+$pythonInterpreterId = 'Paradox.Console.PythonInterpreter'
+$customInterpreterId = 'Paradox.Console.CustomInterpreter'
+
+$nuspecSuffix = '.debug.nuspec'
+$nupkgSuffix = '-alpha.nupkg'
+
+$versionRegex = '(?<=(' + $consoleId + '" version\="|\<version\>))\d.\d.\d'
+$newVersion = Read-Host 'What is the new version?'
+
+# Define functions.
+Function UpdateVersionInFile($fileName)
+{
+	(Get-Content $fileName) | 
+	Foreach-Object {$_ -replace $versionRegex, $newVersion} |
+	Out-File $fileName
+}
+Function GetNuspecFilename($id)
+{
+	return $id + $nuspecSuffix
+}
+Function GetNupkgFilename($id)
+{
+	return $id + '.' + $newVersion + $nupkgSuffix
+}
+
+# Replace version numbers in nuspec files.
+UpdateVersionInFile (GetNuspecFilename $consoleId) $versionRegex $newVersion
+UpdateVersionInFile (GetNuspecFilename $pythonInterpreterId) $versionRegex $newVersion
+UpdateVersionInFile (GetNuspecFilename $customInterpreterId) $versionRegex $newVersion
+
+# Create nuget packages with symbol packages.
+nuget pack (GetNuspecFilename $consoleId) -symbols
+nuget pack (GetNuspecFilename $pythonInterpreterId) -symbols
+nuget pack (GetNuspecFilename $customInterpreterId) -symbols
+
+# Publish nuget packages to nuget.org and symbol packages to symbolsource.org
+nuget push (GetNupkgFilename $consoleId)
+nuget push (GetNupkgFilename $pythonInterpreterId)
+nuget push (GetNupkgFilename $customInterpreterId)
