debe0466a32dbc489731bd3e66e3d2f8f74ed176
Discosultan <j2v@neti.ee>
2015-01-29 16:51:24 +0200

    Renamings.

--- Sample/Paradox.Console.Sample.WindowsStore/Properties/AssemblyInfo.cs
+++ Sample/Paradox.Console.Sample.WindowsStore/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Paradox.Console")]
+[assembly: AssemblyTitle("Paradox.Console.Sample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Paradox.Console")]
+[assembly: AssemblyProduct("Paradox.Console.Sample")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
